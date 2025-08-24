e8589c74e5f65d407ba7225a85282214ef7abb78
Olivier G. <olivierguin33@gmail.com>
2025-08-24 20:42:35 +0200

    initialisation du fichier app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+############################
+# User interface           #
+#                          #
+# Last update : 2025/08/24 #
+############################
+
+
+import streamlit as st
+
+st.title("🎬 Movie Finder (Prototype)")
+st.write("Appli en cours de construction...")
